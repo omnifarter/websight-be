1fe2fd1757d38b91595a68536d82de89a725f064
Gabriel <gabriel@paywithsplit.co>
2020-10-15 23:02:50 +0800

    added in changes to fit changed DB schema

--- src/index.js
+++ src/index.js
@@ -11,65 +11,42 @@ const eraseDatabaseOnSync = false;
   
 connectDb().then(async()=>{
     app.listen(process.env.MONGO_PORT,()=>{
-        console.log('App listening on port ' + process.env.MONGO_PORT )
+        console.log('App listening on port ' + process.env.MONGO_PORT)
     })
 })
 
 
 app.get('/retirement', async (req, res) => {
-    const retirement = await models.retirement.find({category:req.query.category});
+    console.log(req.query.age)
+    const retirement = await models.retirement.find({
+        $or:[ 
+            {$and:
+                [{"Age.0":{ $lte:parseInt(req.query.age)},"Age.1":{$gte:parseInt(req.query.age)}}]
+            },
+            {Age:null}
+        ]});
     return res.send(retirement);
   });
 
 app.get('/housing', async(req,res) =>{
-    let general = await models.housing.find({property:null,property2:null})
-    let property2 = await models.housing.find({property:null,property2:req.query.property2,category2:req.query.category2})
-    let bothProperty = await models.housing.find({
-        property:req.query.property,
-        category:req.query.category,
-        property2:req.query.property2,
-        category2:req.query.category2
-    })
-    let housing = general.concat(property2,bothProperty)
+    const housing = await models.housing.find({ $or: [{ Intent: req.query.intent }, { "Type of flat": req.query.HDBtype }, { "Type of loan": req.query.loan }] })
     return res.send(housing)
 })
 
 app.get('/healthcare',async (req,res)=>{
-    //REQUEST FORMAT:
-    // {
-    //     property:'age',
-    //     category:0,
-    //     property1:'ageGeneration',
-    //     category1:0,
-    //     property2:'ISP',
-    //     category:true,
-    //     property3:'PEC',
-    //     category:true
-    // }
-    let coverage = await models.healthcare.find({
-        property2:req.query.property2,
-        category2:req.query.category2,
-        property3:req.query.property3,
-        category3:req.query.category3,
-        topic:'Coverage'})
-    //get the general one too
-    coverage = coverage.concat(await models.healthcare.find({property:null,property2:null,property3:null,topic:'Coverage'}))
+    const healthcare = await models.healthcare.find({
+        $or:[ 
+            {$and:
+                [{"Age.0":{ $lte:parseInt(req.query.age)},"Age.1":{$gte:parseInt(req.query.age)}}]
+            },
+            {Age:null}
+        ]
+        ,
+         "Has pre-existing condition": {$in:[req.query.PEC,null]},
+         "has IPs": {$in:[req.query.loan,null]},
+        "Generation":{$in:[req.query.generation,null]} 
+    })
 
-    let premiums = await models.healthcare.find({property:null,property2:null,property3:null,topic:'Premiums'})
-    if(req.query.property2){
-        if(req.query.category3){    
-            premiums=premiums.concat(await models.healthcare.find({topic:'Premiums',property:null,category3:true}))
-        }
-        if(req.query.category1!==-1){
-            premiums=premiums.concat(await models.healthcare.find({property:'ageGeneration',category:req.query.category1}))
-        }    
-    }
-
-    let awl = await models.healthcare.find({property:null,topic:'Additional Withdrawal Limits'})
-    awl =awl.concat(await models.healthcare.find({property:req.query.property,category:req.query.category,topic:'Additional Withdrawal Limits'}))
-
-    let results = coverage.concat(premiums,awl)
-
-    return res.send(results)
+    return res.send(healthcare)
 })
 
--- src/models/healthcare.js
+++ src/models/healthcare.js
@@ -1,15 +1,7 @@
 import mongoose from 'mongoose';
  
 const dataSchema = new mongoose.Schema({
-    text:String,
-    type:String,
-    topic:String,
-    property:String,
-    category:String,
-    property2:String,
-    category2:Boolean,
-    property3:String,
-    category3:Boolean,
+    Age:Number
 });
  
 const healthcare = mongoose.model('healthcare', dataSchema,'healthcare',true);
